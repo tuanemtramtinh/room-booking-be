c8efa706b5d29243be6a59fdb6177ad7a1a4ce6d
gbao <bao.pham@palexy.com>
2026-04-21 08:03:23 +0700

    feat: RB-25, RB-27 implement room management CR operations

---
+++ src/main/java/com/hcmut/roombookingbe/repositories/RoomRepository.java
@@ -0,0 +1,9 @@
+package com.hcmut.roombookingbe.repositories;
+
+import com.hcmut.roombookingbe.entities.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends JpaRepository<Room, Long> {
+}
